125c5f6f2eb6968369141b54e5ab628b3f67f892
Aram Yako <aramyako@hotmail.com>
2021-09-08 23:08:57 +0200

    update how to fetch order id

--- AzureFunctions/AzureFunctionDocumentationAzure/EmailLincesFile.cs
+++ AzureFunctions/AzureFunctionDocumentationAzure/EmailLincesFile.cs
@@ -12,31 +12,33 @@ namespace AzureFunctionDocumentationAzure
     public static class EmailLincesFile
     {
         [FunctionName("EmailLincesFile")]
-        public  static void Run([BlobTrigger("licenses/{orderId}", Connection = "BlobStorageConnectionString")]Stream orderBlobStream,
+        public  static void Run([BlobTrigger("licenses/{orderId}.json", Connection = "BlobStorageConnectionString")]Stream orderBlobStream,
             [SendGrid(ApiKey = "SendGridApiKey")] ICollector<SendGridMessage> sender,
             [Table("%TableName%","orders","{orderId}", Connection ="PaymentTableConnectionString")] Order order, //Make lookup in table for us
             string orderId, 
             ILogger log)
         {
 
-            log.LogInformation($"EmailLincesFile triggered");
+            if(order != null)
+            {
+                log.LogInformation($"EmailLincesFile triggered");
 
-            log.LogInformation($"Got order from {order.Email}");
+                log.LogInformation($"Got order from {order.Email}");
 
-            var message = new SendGridMessage();
+                var message = new SendGridMessage();
 
-            message.AddTo(order.Email);
+                message.AddTo(order.Email);
 
-            message.SetFrom(new EmailAddress(Environment.GetEnvironmentVariable("EmailSender")));
+                message.SetFrom(new EmailAddress(Environment.GetEnvironmentVariable("EmailSender")));
 
-            message.SetSubject("Your license file");
-            
-            message.HtmlContent = "Thank you for your order";
+                message.SetSubject("Your license file");
 
-            //if it contains test.com we wont add it, hence to send it
-            if (!order.Email.EndsWith("@test.com"))
-                sender.Add(message);
+                message.HtmlContent = "Thank you for your order";
 
+                //if it contains test.com we wont add it, hence to send it
+                if (!order.Email.EndsWith("@test.com"))
+                    sender.Add(message);
+            }
         }
     }
 }
